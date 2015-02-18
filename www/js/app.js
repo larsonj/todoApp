de0e4f3afda5dda2563d2fa0a6e117fdbc1e3ae9
jcl <john@mygardeninsight.com>
2015-02-18 16:01:27 -0500

    added project code

--- www/js/app.js
+++ www/js/app.js
@@ -1,28 +1,117 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('todoApp', ['ionic'])
 
-.controller('TodoCtrl', function($scope) {
-    $scope.tasks = [
-	{title: 'Collect coins'},
-	{title: 'Eat mushrooms'},
-	{title: 'Get high enough to grap the Flag'},
-	{title: 'Find the Princes!'}
-    ];
+.factory('Projects', function() {
+    return {
+	all: function() {
+	    var projectString = window.localStorage['projects'];
+	    if (projectString) {
+		return angular.fromJson(projectString);
+	    }
+	    return [];
+	},
+	save: function (projects) {
+	    window.localStorage['projects'] = angular.toJson(projects);
+	},
+	newProject: function (projectTitle) {
+	    // add a new project
+	    return {
+		title: projectTitle,
+		tasks: []
+	    };
+	},
+	getLastActiveIndex: function () {
+	    return parseInt(window.localStorage['lastActiveProject']) || 0;
+	},
+	setLastActiveIndex: function (index) {
+	    window.localStorage['lastActiveProject'] = index;
+
+	}
+    }
 })
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+.controller('TodoCtrl', function($scope, $timeout, $ionicModal, Projects, $ionicSideMenuDelegate) {
+    // a utility function for creating a new project
+    // with the given projectTitle
+    var createProject = function(projectTitle) {
+	var newProject = Projects.newProject(projectTitle);
+	$scope.projects.push(newProject);
+	Projects.save($scope.projects);
+	$scope.selectProject(newProject, $scope.projects.length-1);
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
+
+    // load or initialize projects
+    $scope.projects = Projects.all();
+
+    // grap the last active, or the first project
+    $scope.activeProject = $scope.projects[Projects.getLastActiveIndex()];
+
+    // Called to create a new project
+    $scope.newProject = function() {
+	var projectTitle = prompt('Project name');
+	if(projectTitle) {
+	    createProject(projectTitle);
+	}
+    };
+
+    // called to select the given project
+    $scope.selectProject = function(project, index) {
+	$scope.activeProject = project;
+	Projects.setLastActiveIndex(index);
+	$ionicSideMenuDelegate.toggleLeft(false);
+    };
+
+    // Create our model
+    $ionicModal.fromTemplateUrl('new-task.html', function(modal)  {
+	$scope.taskModal = modal;
+    }, {
+	scope: $scope
+
+    });
+    
+    $scope.createTask = function(task) {
+	if(!$scope.activeProject || !task) {
+	    return;
+	}
+	$scope.activeProject.tasks.push({
+	    title: task.title
+	});
+	$scope.taskModal.hide();
+
+	// inefficient but save all the projects
+	Projects.save($scope.projects);
+
+	task.title = "";
+	
+    };
+
+    $scope.newTask = function() {
+	$scope.taskModal.show();
     }
-  });
-})
+
+    $scope.closeNewTask = function() {
+	$scope.taskModal.hide();
+    };
+
+    $scope.toggleProjects = function() {
+	$ionicSideMenuDelegate.toggleLeft();
+    };
+
+    // try to create the first project, make sure to defer 
+    // this by using $timeout so everyting is initialized properly
+
+    $timeout(function() {
+	if($scope.projects.length == 0) {
+	    while(true) {
+		var projectTitle = prompt('your first project title:');
+		if(projectTitle) {
+		    createProject(projectTitle);
+		    break;
+		}
+	    }
+	}
+    });
+
+
+});
+
