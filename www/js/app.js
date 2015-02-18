1c4ce058673c443fe1cbc660f6b7cbbcab3cf2e9
jcl <john@mygardeninsight.com>
2015-02-18 14:25:25 -0500

    wip 5.Creating Tasks - initial version of task list

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,16 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('todoApp', ['ionic'])
+
+.controller('TodoCtrl', function($scope) {
+    $scope.tasks = [
+	{title: 'Collect coins'},
+	{title: 'Eat mushrooms'},
+	{title: 'Get high enough to grap the Flag'},
+	{title: 'Find the Princes!'}
+    ];
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
